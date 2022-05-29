e2d948dd92b3eb0052d2e70e5aab505643509a4d
ivanaLehki <01013464@edu.campus02.at>
2022-05-29 22:27:30 +0200

    Added JUnit Test and updated the exercise4 markdown

---
+++ exercise4.md
@@ -0,0 +1,14 @@
+# Exercise 4: Source Code Testing
+
+### In this Exercise I have written JUnit Test for the implementes Methods in the Calculator Class.
+
+### Each Method was tested with 2 different types of test approach:
+    * once with the prepared result
+    * once with the output from the method in the Calcultor class
+
+
+## Coverage
+### Tests have passed with the Coverage of 100%
+## ![ex4_1](src/main/resources/images/ex4_1.PNG)
+
+
---
+++ src/test/java/at/campus02/bsd/calculator/CalcTest.java
@@ -0,0 +1,55 @@
+package at.campus02.bsd.calculator;
+
+import at.campus02.bsd.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+
+public class CalcTest
+{
+    Calculator calc;
+
+
+    @BeforeEach
+    public void setup() throws IOException{
+        calc = new Calculator();
+    }
+
+    double result;
+    @Test
+    public void add(){
+        Assertions.assertEquals(8, 5+3);
+
+        result = 5 + 3;
+        Assertions.assertEquals(result, calc.add(5, 3));
+    }
+
+    @Test
+    public void multiply(){
+        Assertions.assertEquals(15, 5*3);
+
+        result = 5 * 3;
+        Assertions.assertEquals(result, calc.multiply(5, 3));
+    }
+
+    @Test
+    public void divide(){
+        Assertions.assertEquals(5, 15/3);
+
+        result = 15 / 3;
+        Assertions.assertEquals(result, calc.divide(15, 3));
+    }
+
+    @Test
+    public void minus(){
+        Assertions.assertEquals(2, 5-3);
+
+        result = 5 - 3;
+        Assertions.assertEquals(result, calc.minus(5, 3));
+    }
+
+
+
+}
